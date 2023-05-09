1d879aa138df2739e3e22b43d413805d8c03af78
Vishal Kharade <vishalkharade@icloud.com>
2023-05-10 00:47:42 +0530

    Finished the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {
+  FlatList,
+  Pressable,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  ViewBase,
+} from 'react-native';
 import React, {useState} from 'react';
 import Snackbar from 'react-native-snackbar';
+import {currencyByRupee} from './constants';
+import CurrencyButton from './components/CurrencyButton';
 
 const App = () => {
   const [inputValue, setInputValue] = useState('');
@@ -15,28 +26,117 @@ const App = () => {
         textColor: '#000000',
       });
     }
-    // const inputAmount = parseFloat(inputValue);
-    // if (!isNaN(inputAmount)) {
-    //   const convertedValue = inputAmount * targetValue.value;
-    //   const result = `${targetValue.symbol} ${convertedValue.toFixed(2)} 🤑`;
-    //   setResultValue(result);
-    //   setTargetCurrency(targetValue.name);
-    // } else {
-    //   return Snackbar.show({
-    //     text: 'Not valid number to convert',
-    //     backgroundColor: '#EA7773',
-    //     textColor: '#000000',
-    //   });
-    // }
+    const inputAmount = parseFloat(inputValue);
+    if (!isNaN(inputAmount)) {
+      const convertedValue = inputAmount * targetValue.value;
+      const result = `${targetValue.symbol} ${convertedValue.toFixed(2)} 🤑`;
+      setResultValue(result);
+      setTargetCurrency(targetValue.name);
+    } else {
+      return Snackbar.show({
+        text: 'Not valid number to convert',
+        backgroundColor: '#EA7773',
+        textColor: '#000000',
+      });
+    }
   };
 
   return (
-    <SafeAreaView>
-      <Text>App</Text>
-    </SafeAreaView>
+    <>
+      <View style={styles.container}>
+        <View style={styles.topContainer}>
+          <View style={styles.rupeesContainer}>
+            <Text style={styles.rupee}>₹</Text>
+            <TextInput
+              maxLength={14}
+              value={inputValue}
+              clearButtonMode="always" //only for ios
+              onChangeText={setInputValue}
+              keyboardType="number-pad"
+              placeholder="Enter amount in Rupees"
+            />
+          </View>
+          {resultValue && <Text style={styles.resultTxt}>{resultValue}</Text>}
+        </View>
+        <View style={styles.bottomContainer}>
+          <FlatList
+            numColumns={1}
+            data={currencyByRupee}
+            keyExtractor={item => item.name}
+            renderItem={({item}) => (
+              <Pressable
+                style={[
+                  styles.button,
+                  targetCurrency === item.name && styles.selected,
+                ]}
+                onPress={() => buttonPressed(item)}>
+                <CurrencyButton {...item} />
+              </Pressable>
+            )}
+          />
+        </View>
+      </View>
+    </>
   );
 };
 
 export default App;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#515151',
+  },
+  topContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+  resultTxt: {
+    fontSize: 32,
+    color: '#000000',
+    fontWeight: '800',
+  },
+  rupee: {
+    marginRight: 8,
+
+    fontSize: 22,
+    color: '#000000',
+    fontWeight: '800',
+  },
+  rupeesContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  inputAmountField: {
+    height: 40,
+    width: 200,
+    padding: 8,
+    borderWidth: 1,
+    borderRadius: 4,
+    backgroundColor: '#FFFFFF',
+  },
+  bottomContainer: {
+    flex: 3,
+  },
+  button: {
+    flex: 1,
+
+    margin: 12,
+    height: 60,
+
+    borderRadius: 12,
+    backgroundColor: '#fff',
+    elevation: 2,
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowColor: '#333',
+    shadowOpacity: 0.1,
+    shadowRadius: 1,
+  },
+  selected: {
+    backgroundColor: '#ffeaa7',
+  },
+});
